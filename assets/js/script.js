694c63fc52ae0ee1d82ad4ddc4c71728bbd676a6
Vepp1 <vinipontarolla@gmail.com>
2022-09-08 14:44:54 +0000

    Add displayException function, to pop a modal showing the API error

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,9 +5,26 @@ const resultsModal = new bootstrap.Modal(document.getElementById('resultsModal')
 document.getElementById('status').addEventListener('click', e => getStatus(e));
 document.getElementById('submit').addEventListener('click', e => postForm(e));
 
+function processOptions(form) {
+
+    let optArray = [];
+
+    for (let entry of form.entries()) {
+        if(entry[0] === 'options') {
+            optArray.push(entry[1]);
+        }
+    }
+
+    form.delete('options');
+
+    form.append('options', optArray.join());
+
+    return form;
+
+}
 
 async function postForm(e) {
-    const form = new FormData(document.getElementById('checksform'));
+    const form = processOptions(new FormData(document.getElementById('checksform')));
 
     const response = await fetch(API_URL, {
         method: "POST",
@@ -22,8 +39,10 @@ async function postForm(e) {
     if (response.ok) {
         displayErrors(data);
     } else {
+        displayException(data);
         throw new Error(data.error);
     }
+
 }
 
 function displayErrors(data) {
@@ -34,9 +53,9 @@ function displayErrors(data) {
     } else {
         results = `<div>Total Errors: <span class='error_count'>${data.total_errors}</span></div>'`;
         for (let error of data.error_list) {
-            results += `<div> At line <span class='line'>${error.line}</span>, `
-            results += `column <span class='column'>${error.col}</span></div>`
-            results += `<div class='error'>${error.error}</div>`
+            results += `<div> At line <span class='line'>${error.line}</span>, `;
+            results += `column <span class='column'>${error.col}</span></div>`;
+            results += `<div class='error'>${error.error}</div>`;
         }
     }
 
@@ -54,12 +73,13 @@ async function getStatus(e) {
     const data = await response.json();
 
     if (response.ok) {
-        displayStatus(data)
+        displayStatus(data);
     } else {
+        displayException(data);
         throw new Error(data.error);
     }
 }
-
+;
 function displayStatus(data) {
     let heading = 'Api Key Status';
     let results = `<div>Your key is valid until</div>`;
@@ -67,6 +87,20 @@ function displayStatus(data) {
 
     document.getElementById('resultsModalTitle').innerText = heading;
     document.getElementById('results-content').innerHTML = results
+
+    resultsModal.show();
+}
+
+function displayException(data) {
+    let heading = 'An Exception Ocurred';
+
+    results = `<div>The API returned status code:${data.status_code}</div> `;
+    results += `<div>Error num: <strong>${data.error_no}</strong></div>`;
+    results += `<div>Error text: <strong>${data.error}</strong></div>`;
+    
+
+    document.getElementById('resultsModalTitle').innerText = heading;
+    document.getElementById('results-content').innerHTML = results;
 
     resultsModal.show();
 }
